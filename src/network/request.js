e73a75427397d246d9c411729d3247aaf80127a2
yingzishuai <55782823+yingzishuai@users.noreply.github.com>
2020-12-03 11:13:23 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 创建实例
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/h8',
+    baseURL: 'http://123.207.32.32:8000/',
     timeout: 5000
   })
   // 请求拦截
